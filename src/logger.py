b251e3cf5f24f551d65e585f7731e1fdd2247411
Marco Stucchi <marco.stucchi@gmail.com>
2020-03-15 00:13:45 +0100

    Graphs displayed with a two-columns layout

--- src/logger.py
+++ src/logger.py
@@ -3,16 +3,20 @@ import json
 import matplotlib
 import matplotlib.pyplot as plt
 
-#URLS
+# URLS
 urlNazione = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json'
 urlProvince = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json'
 
+# Graph constants
+SINGLE_GRAPH = 0
+TWO_COLUMNS_GRAPH_1 = 121
+TWO_COLUMNS_GRAPH_2 = 122
 
 # ------------------------------------------------------- Casi totali Italia
 
-def plot_nazione():
-# Get data file
+def plot_nazione(graphType):
 
+    # Get data file
     req = urllib.request.Request(urlNazione)
     r = urllib.request.urlopen(req).read()
 
@@ -24,18 +28,23 @@ def plot_nazione():
     for dic in data:
         total.append(int(dic['totale_casi']))
 
+    # Decide about plot type 
+    if graphType != SINGLE_GRAPH:
+        plt.subplot(graphType)
+
     # Plot Data
     plt.plot(total, marker='o', markersize=4)
     plt.title('Casi Totali Italia')
     plt.grid(True)
-    plt.show()
+    if graphType == SINGLE_GRAPH:
+        plt.show()
 
 # ------------------------------------------------------- Casi totali province
 
 def search(provincia, dic):
     return [element['totale_casi'] for element in dic if element['denominazione_provincia'] == provincia]
 
-def plot_province():
+def plot_province(graphType):
 
     province = [ 'Bergamo', 'Milano', 'Brescia', 'Roma', 'Napoli', 'Lodi']
 
@@ -45,20 +54,26 @@ def plot_province():
 
     # Parse response
     data = json.loads(r.decode('utf-8'))
+    
+    # Decide about plot type 
+    if graphType != SINGLE_GRAPH:
+        plt.subplot(graphType)
 
-    # Extracta data
+    # Extract data
     for provincia in province:
         data_provincia = search(provincia, data)
         plt.plot(data_provincia, label=provincia, marker='o', markersize=4)
-        # print(data_provincia)
 
     # Plot data
     plt.legend()
     plt.title('Casi Totali Province')
     plt.grid(True)
-    plt.show()
+    if graphType == SINGLE_GRAPH:
+        plt.show()    
 
+# ------------------------------------------------------- Main
 
 if __name__ == '__main__':
-    #plot_nazione()
-    plot_province()
+    plot_nazione(TWO_COLUMNS_GRAPH_1)
+    plot_province(TWO_COLUMNS_GRAPH_2)
+    plt.show()
